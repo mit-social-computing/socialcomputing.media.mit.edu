4319f2e21da6c9783197fd05577539755d6847fe
Brian Whitton <me@brianwhitton.com>
2014-12-01 20:58:59 -0500

    build

--- public/assets/js/scripts.concat.js
+++ public/assets/js/scripts.concat.js
@@ -17493,67 +17493,6 @@ if ( typeof define === 'function' && define.amd ) {
  
 })(window.Zepto || window.jQuery);
 
-/*global location,setTimeout*/
-// chapters.js
-
-$(function() {
-    var hash = location.hash
-
-    if ( hash ) {
-        // need to trigger on gallery layout
-        // use browserify to creat bundles and pass event
-        // for now just Timeout
-        setTimeout(function() {
-            $('#scrollable').animate({
-                scrollTop : $(hash)[0].offsetTop -
-                    ( parseInt(getComputedStyle($('#content')[0]).paddingTop, 10) +
-                      $('#siteHeader').height() )
-            })
-        }, 100)
-
-        // only for non-blog pages
-        $('#chapterNav').find('a').not('[href=' + hash + ']').removeClass('active')
-        $('#chapterNav').find('[href=' + hash + ']').addClass('active')
-    }
-
-    $('#sidenavButton').click(function(e) {
-        $(this).toggleClass('close')
-        var open = $('#sidebar').attr('data-sidebar') === 'is-open',
-            width = 0
-
-        if ( !open ) {
-            $('#chapterList').find('li').each(function(e) {
-                var textWidth = $(this).text().length * 7
-                if ( textWidth > width ) {
-                    width = textWidth
-                }
-            })
-            $('#sidenav, #sidebar').width(width > 399 ? width : 399)
-        } else {
-            $('#sidenav, #sidebar').innerWidth(56)
-        }
-
-        $('#sidebar').attr('data-sidebar', open ? 'is-closed' : 'is-open')
-    })
-
-    $('#chapterNav').on('click', 'a', function(e) {
-        e.preventDefault()
-
-        var scrollTo = $(this.hash)[0].offsetTop -
-            ( parseInt(getComputedStyle($('#content')[0]).paddingTop, 10) +
-              $('#siteHeader').height() ),
-            selector = '[href=' + this.hash +']'
-
-        history.pushState({}, '', this.hash)
-        $('#scrollable').animate({
-            scrollTop : scrollTo
-        })
-
-        $(e.delegateTarget).find('a').not(selector).removeClass('active')
-        $(e.delegateTarget).find(selector).addClass('active')
-    })
-})
-
 // gallery.js
 
 $(function() {
@@ -17568,6 +17507,27 @@ $(function() {
     })
 
     $gallery.slickBox({ 'data-group': 'block' })
+})
+
+// global.js
+
+$(function() {
+
+    // scrollbar check
+    function scrollbarGlobalDetect() {
+        var contentHeight = $('#content').children()
+                .map(function() { return $(this).outerHeight() })
+                .get()
+                .reduce(function(a,b) { return a + b }),
+            pageHeight = $('.scrollbar').height()
+
+        $('body').toggleClass('is-not-scrolling', contentHeight < pageHeight)
+    }
+
+    if ( !!$('.scrollbar').length ) {
+        $(window).on( 'resize', _.debounce(scrollbarGlobalDetect, 100) )
+        scrollbarGlobalDetect()
+    }
 })
 
 /*global location,history*/
@@ -18178,6 +18138,42 @@ $(function() {
 // nav.js
 
 $(function() {
+    var hash = location.hash
+
+    if ( hash ) {
+        // need to trigger on gallery layout
+        // use browserify to creat bundles and pass event
+        // for now just Timeout
+        setTimeout(function() {
+            $('#scrollable').animate({
+                scrollTop : $(hash)[0].offsetTop -
+                    ( parseInt(getComputedStyle($('#content')[0]).paddingTop, 10) +
+                      $('#siteHeader').height() )
+            })
+        }, 100)
+
+        // only for non-blog pages
+        $('#chapterNav').find('a').not('[href=' + hash + ']').removeClass('active')
+        $('#chapterNav').find('[href=' + hash + ']').addClass('active')
+    }
+
+    $('#chapterNav').on('click', 'a', function(e) {
+        e.preventDefault()
+
+        var scrollTo = $(this.hash)[0].offsetTop -
+            ( parseInt(getComputedStyle($('#content')[0]).paddingTop, 10) +
+              $('#siteHeader').height() ),
+            selector = '[href=' + this.hash +']'
+
+        history.pushState({}, '', this.hash)
+        $('#scrollable').animate({
+            scrollTop : scrollTo
+        })
+
+        $(e.delegateTarget).find('a').not(selector).removeClass('active')
+        $(e.delegateTarget).find(selector).addClass('active')
+    })
+
     $('#menu').click(function() {
         $(this).toggleClass('close')
         $('body').toggleClass('is-menu-open')
@@ -18191,6 +18187,26 @@ $(function() {
         } else {
             return true
         }
+    })
+
+    $('#sidenavButton').click(function(e) {
+        $(this).toggleClass('close')
+        var open = $('#sidebar').attr('data-sidebar') === 'is-open',
+            width = 0
+
+        if ( !open ) {
+            $('#chapterList').find('li').each(function(e) {
+                var textWidth = $(this).text().length * 7
+                if ( textWidth > width ) {
+                    width = textWidth
+                }
+            })
+            $('#sidenav, #sidebar').innerWidth(width > 399 ? width : 399)
+        } else {
+            $('#sidenav, #sidebar').innerWidth(56)
+        }
+
+        $('#sidebar').attr('data-sidebar', open ? 'is-closed' : 'is-open')
     })
 })
 
