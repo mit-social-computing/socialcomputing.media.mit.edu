beb7d6bc55e3984b5bc40906b1201d08c71493ab
Brian Whitton <me@brianwhitton.com>
2014-12-04 16:24:18 -0500

    fading in grid images on progress, same for galleries. using image as mask

--- public/assets/js/scripts.concat.js
+++ public/assets/js/scripts.concat.js
@@ -17543,17 +17543,29 @@ if ( typeof define === 'function' && define.amd ) {
 // gallery.js
 
 $(function() {
-    $gallery = $('.gallery').imagesLoaded(function() {
-        $gallery.packery({
-            itemSelector : '.gallery-item',
-            gutter : 10,
-            rowHeight : 207,
-            transitionDuration : 0
+    var $gallery = $('.gallery'),
+        $grid = $('.grid')
+
+    $gallery.imagesLoaded()
+        .always(function() {
+            $gallery.packery({
+                itemSelector : '.gallery-item',
+                gutter : 10,
+                rowHeight : 207,
+                transitionDuration : 0
+            })
+            $gallery.packery('bindResize')
         })
-        $gallery.packery('bindResize')
-    })
+        .progress( function(instance, image) {
+            image.img.classList.add('loaded')
+        })
 
     $gallery.slickBox({ 'data-group': 'block' })
+
+    $grid.imagesLoaded()
+        .progress(function(instance, image) {
+            image.img.classList.add('loaded')
+        })
 })
 
 // global.js
--- source/js/gallery.js
+++ source/js/gallery.js
@@ -1,15 +1,27 @@
 // gallery.js
 
 $(function() {
-    $gallery = $('.gallery').imagesLoaded(function() {
-        $gallery.packery({
-            itemSelector : '.gallery-item',
-            gutter : 10,
-            rowHeight : 207,
-            transitionDuration : 0
+    var $gallery = $('.gallery'),
+        $grid = $('.grid')
+
+    $gallery.imagesLoaded()
+        .always(function() {
+            $gallery.packery({
+                itemSelector : '.gallery-item',
+                gutter : 10,
+                rowHeight : 207,
+                transitionDuration : 0
+            })
+            $gallery.packery('bindResize')
         })
-        $gallery.packery('bindResize')
-    })
+        .progress( function(instance, image) {
+            image.img.classList.add('loaded')
+        })
 
     $gallery.slickBox({ 'data-group': 'block' })
+
+    $grid.imagesLoaded()
+        .progress(function(instance, image) {
+            image.img.classList.add('loaded')
+        })
 })
